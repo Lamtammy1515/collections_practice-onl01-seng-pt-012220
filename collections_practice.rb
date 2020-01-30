ca49022c76960078e33d51695b3ae171efd2071d
Tammy Lam <lamtammy1515@gmail.com>
2020-01-30 05:10:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,4 +46,13 @@ def sum_array(num)
   num.reduce(&:+)
 end 
 
+def add_s(array)
+      array.each_with_index.collect do |word,index|
+        if index != 1 
+          word = word + "s"
+        else
+          word = word
+        end
+  end
+end
 
