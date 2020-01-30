5b2778c8deefb75ccf355afa1493c8a78009c419
Tammy Lam <lamtammy1515@gmail.com>
2020-01-30 04:00:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,13 +12,5 @@ end
 
 def sort_array_char_count(animals)
   animals = ["dogs", "cat", "Horses"]
-  animals.sort do |a, b|
-  if a == b
-    0
-  elsif a > b
-    1
-  elsif a < b
-    -1
-  end
-end 
+  animals
 end 
